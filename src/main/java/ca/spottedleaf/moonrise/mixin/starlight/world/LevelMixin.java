8f88b1bea1746a347a85d8ff2c2c051101100f78
Spottedleaf <Spottedleaf@users.noreply.github.com>
2023-10-16 07:01:11 -0700

    Add default implementation for ExtendedWorld in Level
    This should hopefully help mod compatibility with mods that
    implement their own Levels.
    
    Fixes https://github.com/PaperMC/Starlight/issues/187

--- src/main/java/ca/spottedleaf/moonrise/mixin/starlight/world/LevelMixin.java
+++ src/main/java/ca/spottedleaf/moonrise/mixin/starlight/world/LevelMixin.java
@@ -3,7 +3,21 @@ package ca.spottedleaf.moonrise.mixin.starlight.world;
 import ca.spottedleaf.moonrise.patches.starlight.world.StarlightWorld;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.ChunkStatus;
+import net.minecraft.world.level.chunk.LevelChunk;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(Level.class)
-public abstract class LevelMixin implements LevelAccessor, AutoCloseable, StarlightWorld {}
+public abstract class LevelMixin implements LevelAccessor, AutoCloseable, StarlightWorld {
+
+    @Override
+    public LevelChunk getChunkAtImmediately(final int chunkX, final int chunkZ) {
+        return this.getChunkSource().getChunk(chunkX, chunkZ, false);
+    }
+
+    @Override
+    public ChunkAccess getAnyChunkImmediately(final int chunkX, final int chunkZ) {
+        return this.getChunkSource().getChunk(chunkX, chunkX, ChunkStatus.EMPTY, false);
+    }
+}
