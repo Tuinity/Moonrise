adad376255cf79476accd055a33c16ec96bcd2f3
Spottedleaf <Spottedleaf@users.noreply.github.com>
2024-05-22 14:23:15 -0700

    Optimise BlockSoil nearby water lookup
    Ported from Paper

---
+++ src/main/java/ca/spottedleaf/moonrise/mixin/farm_block/FarmBlockMixin.java
@@ -0,0 +1,48 @@
+package ca.spottedleaf.moonrise.mixin.farm_block;
+
+import ca.spottedleaf.moonrise.patches.chunk_getblock.GetBlockChunk;
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.FarmBlock;
+import net.minecraft.world.level.chunk.ChunkSource;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.chunk.status.ChunkStatus;
+import net.minecraft.world.level.material.FluidState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(FarmBlock.class)
+public abstract class FarmBlockMixin {
+
+    /**
+     * @reason Avoid usage of betweenClsed, this can become very hot when
+     *         there are significant numbers of farm blocks in the world
+     * @author Spottedleaf
+     */
+    @Overwrite
+    private static boolean isNearWater(final LevelReader world0, final BlockPos pos) {
+        final ChunkSource chunkCache = ((Level)world0).getChunkSource();
+        final int xOff = pos.getX();
+        final int yOff = pos.getY();
+        final int zOff = pos.getZ();
+
+        for (int dz = -4; dz <= 4; ++dz) {
+            final int z = dz + zOff;
+            for (int dx = -4; dx <= 4; ++dx) {
+                final int x = xOff + dx;
+                final LevelChunk chunk = (LevelChunk)chunkCache.getChunk(x >> 4, z >> 4, ChunkStatus.FULL, true);
+                for (int dy = 0; dy <= 1; ++dy) {
+                    final int y = dy + yOff;
+                    final FluidState fluid = ((GetBlockChunk)chunk).moonrise$getBlock(x, y, z).getFluidState();
+                    if (!fluid.isEmpty() && fluid.is(FluidTags.WATER)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
