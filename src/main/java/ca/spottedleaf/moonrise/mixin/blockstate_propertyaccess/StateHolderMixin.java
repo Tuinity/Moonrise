dbbf10d2218be15cc4ffbec8e86c289ed90b1e79
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-08-27 17:50:41 -0400

    remove newline

--- src/main/java/ca/spottedleaf/moonrise/mixin/blockstate_propertyaccess/StateHolderMixin.java
+++ src/main/java/ca/spottedleaf/moonrise/mixin/blockstate_propertyaccess/StateHolderMixin.java
@@ -14,7 +14,6 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
