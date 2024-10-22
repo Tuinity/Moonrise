e69eeba47c1c7e12080e077b370fdc27fe8da351
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-10-22 12:38:20 -0700

    Use Moonrise executor for section occlusion tasks

---
+++ src/main/java/ca/spottedleaf/moonrise/mixin/render/SectionOcclusionGraphMixin.java
@@ -0,0 +1,43 @@
+package ca.spottedleaf.moonrise.mixin.render;
+
+import ca.spottedleaf.concurrentutil.executor.thread.PrioritisedThreadPool;
+import ca.spottedleaf.concurrentutil.util.Priority;
+import ca.spottedleaf.moonrise.common.util.MoonriseCommon;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import net.minecraft.client.renderer.SectionOcclusionGraph;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(SectionOcclusionGraph.class)
+abstract class SectionOcclusionGraphMixin {
+
+    @Unique
+    private static final PrioritisedThreadPool.ExecutorGroup.ThreadPoolExecutor SECTION_OCCLUSION_EXECUTOR = MoonriseCommon.SECTION_OCCLUSION_EXECUTOR_GROUP.createExecutor(
+        -1, MoonriseCommon.WORKER_QUEUE_HOLD_TIME, 0
+    );
+
+    /**
+     * @reason Change executor to use our thread pool
+     *         Note: even at normal priority, our worker pool will try to share resources equally rather than having it
+     *         be a free-for-all
+     * @author jpenilla
+     */
+    @Redirect(
+        method = "scheduleFullUpdate",
+        at = @At(
+            value = "INVOKE",
+            target = "Ljava/util/concurrent/CompletableFuture;runAsync(Ljava/lang/Runnable;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;"
+        )
+    )
+    private CompletableFuture<Void> changeExecutor(final Runnable runnable, final Executor executor) {
+        return CompletableFuture.runAsync(
+            runnable,
+            (final Runnable task) -> {
+                SECTION_OCCLUSION_EXECUTOR.queueTask(task, Priority.NORMAL);
+            }
+        );
+    }
+}
