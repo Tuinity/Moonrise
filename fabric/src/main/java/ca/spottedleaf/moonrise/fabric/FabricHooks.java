9944946b29ef7edb17fec72e49fcd499241e6d96
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-10-26 09:51:34 -0700

    Update Fabric API and call ServerChunkEvents.CHUNK_GENERATE

--- fabric/src/main/java/ca/spottedleaf/moonrise/fabric/FabricHooks.java
+++ fabric/src/main/java/ca/spottedleaf/moonrise/fabric/FabricHooks.java
@@ -19,6 +19,7 @@ import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.ImposterProtoChunk;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.chunk.ProtoChunk;
 import net.minecraft.world.level.entity.EntityTypeTest;
@@ -90,6 +91,9 @@ public final class FabricHooks implements PlatformHooks {
     public void chunkFullStatusComplete(final LevelChunk newChunk, final ProtoChunk original) {
         if (HAS_FABRIC_LIFECYCLE_EVENTS) {
             ServerChunkEvents.CHUNK_LOAD.invoker().onChunkLoad((ServerLevel) newChunk.getLevel(), newChunk);
+            if (!(original instanceof ImposterProtoChunk)) {
+                ServerChunkEvents.CHUNK_GENERATE.invoker().onChunkGenerate((ServerLevel)newChunk.getLevel(), newChunk);
+            }
         }
     }
 
