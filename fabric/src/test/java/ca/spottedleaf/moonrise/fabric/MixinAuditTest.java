43164acf5e6fe5f11a6ca48a0e4fab3186ab07b0
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-11-23 22:35:39 -0700

    Add Mixin audit unit test for Fabric
    Doing this for NeoForge may need to wait until we build using ModDevGradle

---
+++ fabric/src/test/java/ca/spottedleaf/moonrise/fabric/MixinAuditTest.java
@@ -0,0 +1,20 @@
+package ca.spottedleaf.moonrise.fabric;
+
+import net.minecraft.SharedConstants;
+import net.minecraft.server.Bootstrap;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+
+public class MixinAuditTest {
+    @BeforeAll
+    static void beforeAll() {
+        SharedConstants.tryDetectVersion();
+        Bootstrap.bootStrap();
+    }
+
+    @Test
+    void auditMixins() {
+        MixinEnvironment.getCurrentEnvironment().audit();
+    }
+}
