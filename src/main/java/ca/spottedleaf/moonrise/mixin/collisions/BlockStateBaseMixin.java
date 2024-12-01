3624d261f827deae3c32d57079565f69bf0e8c05
Spottedleaf <Spottedleaf@users.noreply.github.com>
2024-12-01 11:45:32 -0800

    Adjust constant collision shape determination
    Our previous hack did not actually catch every case. For now,
    it will only assume a constant collision shape of EMPTY for
    air blocks.
    
    Fixes https://github.com/PaperMC/Paper/issues/11697

--- src/main/java/ca/spottedleaf/moonrise/mixin/collisions/BlockStateBaseMixin.java
+++ src/main/java/ca/spottedleaf/moonrise/mixin/collisions/BlockStateBaseMixin.java
@@ -5,16 +5,12 @@ import ca.spottedleaf.moonrise.patches.collisions.shape.CollisionVoxelShape;
 import com.mojang.serialization.MapCodec;
 import it.unimi.dsi.fastutil.HashCommon;
 import it.unimi.dsi.fastutil.objects.Reference2ObjectArrayMap;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateHolder;
 import net.minecraft.world.level.block.state.properties.Property;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
@@ -32,7 +28,7 @@ abstract class BlockStateBaseMixin extends StateHolder<Block, BlockState> implem
     protected BlockBehaviour.BlockStateBase.Cache cache;
 
     @Shadow
-    public abstract VoxelShape getCollisionShape(BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext);
+    public abstract boolean isAir();
 
     protected BlockStateBaseMixin(Block object, Reference2ObjectArrayMap<Property<?>, Comparable<?>> reference2ObjectArrayMap, MapCodec<BlockState> mapCodec) {
         super(object, reference2ObjectArrayMap, mapCodec);
@@ -93,10 +89,9 @@ abstract class BlockStateBaseMixin extends StateHolder<Block, BlockState> implem
     private void initCollisionState(final CallbackInfo ci) {
         if (this.cache != null) {
             final VoxelShape collisionShape = this.cache.collisionShape;
-            try {
-                this.constantCollisionShape = this.getCollisionShape(null, null, null);
-            } catch (final Throwable throwable) {
-                // :(
+            if (this.isAir()) {
+                this.constantCollisionShape = Shapes.empty();
+            } else {
                 this.constantCollisionShape = null;
             }
             this.occludesFullBlock = ((CollisionVoxelShape)collisionShape).moonrise$occludesFullBlock();
