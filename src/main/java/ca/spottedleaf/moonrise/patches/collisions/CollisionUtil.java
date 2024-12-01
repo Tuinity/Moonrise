1a077389c21a7f211da3879a0ba34b8b0050e122
Spottedleaf <Spottedleaf@users.noreply.github.com>
2024-12-01 11:36:16 -0800

    Force LazyEntityCollisionContext#getEntity() to delegate
    By delegating when the entity is retrieved, we can correctly catch
    cases where the collision method is inspecting some entity state.

--- src/main/java/ca/spottedleaf/moonrise/patches/collisions/CollisionUtil.java
+++ src/main/java/ca/spottedleaf/moonrise/patches/collisions/CollisionUtil.java
@@ -2168,8 +2168,14 @@ public final class CollisionUtil {
 
         public CollisionContext getDelegate() {
             this.delegated = true;
-            final Entity entity = this.getEntity();
+            final Entity entity = super.getEntity();
             return this.delegate == null ? this.delegate = (entity == null ? CollisionContext.empty() : CollisionContext.of(entity)) : this.delegate;
+        }
+
+        @Override
+        public Entity getEntity() {
+            this.getDelegate();
+            return super.getEntity();
         }
 
         @Override
