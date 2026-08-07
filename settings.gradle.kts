c8490c7819a1eb00d3f5de0b3e6921d50261ad71
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2026-08-07 08:41:10 -0700

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,9 +24,9 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("xyz.jpenilla.quiet-fabric-loom-repositories") version "1.17.338"
+    id("xyz.jpenilla.quiet-fabric-loom-repositories") version "1.17.342"
     // https://projects.neoforged.net/neoforged/moddevgradle
-    id("net.neoforged.moddev.repositories") version "2.0.141"
+    id("net.neoforged.moddev.repositories") version "2.0.143"
 }
 
 dependencyResolutionManagement {
