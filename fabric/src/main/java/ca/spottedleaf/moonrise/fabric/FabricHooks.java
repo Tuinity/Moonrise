12f7d323df0fb3237e314c08d5167e705214e155
Newwind <support@newwindserver.com>
2026-09-10 12:16:13 +0100

    Update FabricHooks

--- fabric/src/main/java/ca/spottedleaf/moonrise/fabric/FabricHooks.java
+++ fabric/src/main/java/ca/spottedleaf/moonrise/fabric/FabricHooks.java
@@ -184,32 +184,32 @@ public final class FabricHooks extends BaseChunkSystemHooks implements PlatformH
     }
 
     @Override
-    public boolean configAutoConfigSendDistance() {
+    public boolean configAutoConfigSendDistance(final ServerLevel world) {
         return ConfigHolder.getConfig().chunkLoading.advanced.autoConfigSendDistance;
     }
 
     @Override
-    public double configPlayerMaxLoadRate() {
+    public double configPlayerMaxLoadRate(final ServerLevel world) {
         return ConfigHolder.getConfig().chunkLoading.basic.playerMaxLoadRate;
     }
 
     @Override
-    public double configPlayerMaxGenRate() {
+    public double configPlayerMaxGenRate(final ServerLevel world) {
         return ConfigHolder.getConfig().chunkLoading.basic.playerMaxGenRate;
     }
 
     @Override
-    public double configPlayerMaxSendRate() {
+    public double configPlayerMaxSendRate(final ServerLevel world) {
         return ConfigHolder.getConfig().chunkLoading.basic.playerMaxSendRate;
     }
 
     @Override
-    public int configPlayerMaxConcurrentLoads() {
+    public int configPlayerMaxConcurrentLoads(final ServerLevel world) {
         return ConfigHolder.getConfig().chunkLoading.advanced.playerMaxConcurrentChunkLoads;
     }
 
     @Override
-    public int configPlayerMaxConcurrentGens() {
+    public int configPlayerMaxConcurrentGens(final ServerLevel world) {
         return ConfigHolder.getConfig().chunkLoading.advanced.playerMaxConcurrentChunkGenerates;
     }
 
